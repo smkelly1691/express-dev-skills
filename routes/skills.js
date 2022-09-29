8e0c5161cdc8b66a992661e8dc8712f1b561b439
smkelly1691 <skelly1691@gmail.com>
2022-09-28 19:48:44 -0700

    adds new post router

--- routes/skills.js
+++ routes/skills.js
@@ -6,5 +6,6 @@ const skillsCtrl = require('../controllers/skills')
 router.get('/', skillsCtrl.index)
 router.get('/new', skillsCtrl.new)
 router.get('/:id', skillsCtrl.show)
+router.post('/', skillsCtrl.create)
 
 module.exports = router;
